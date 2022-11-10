e854cbe82be4aff44b9584f73bdae08e838b46e0
onishi taisei <taisei.developer@gmail.com>
2022-11-10 21:15:19 +0900

    🚀 feat: app.dart作成

---
+++ lib/app.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WeTravel(),
+    );
+  }
+}
+
+class WeTravel extends StatelessWidget {
+  const WeTravel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('WeTravel'),
+      ),
+    );
+  }
+}
