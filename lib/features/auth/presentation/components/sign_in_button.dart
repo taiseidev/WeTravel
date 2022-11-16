245c73a958ec084b2b6d83ee32ac9107491bccbc
onishi taisei <taisei.developer@gmail.com>
2022-11-16 20:53:50 +0900

    🚀 feat: 新規会員登録用のテンプレートボタンを作成

---
+++ lib/features/auth/presentation/components/sign_in_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({
+    super.key,
+    required this.callback,
+    required this.text,
+    required this.icon,
+  });
+
+  final VoidCallback callback;
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: callback,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.black54,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon),
+            const Gap(10),
+            Text(text),
+          ],
+        ),
+      ),
+    );
+  }
+}
