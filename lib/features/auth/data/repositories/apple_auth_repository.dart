13313dddb7d6f13a8f78a3ecda5d8b31b3b6886c
onishi taisei <taisei.developer@gmail.com>
2022-11-20 07:21:41 +0900

    🚀 feat: AppleSignIn実装

---
+++ lib/features/auth/data/repositories/apple_auth_repository.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:we_travel/features/auth/data/repositories/sign_in_repository.dart';
+
+import '../../domain/interfaces/i_auth_repository.dart';
+
+part 'apple_auth_repository.g.dart';
+
+@riverpod
+AppleAuthRepository appleAuthRepository(AppleAuthRepositoryRef ref) =>
+    AppleAuthRepository(ref);
+
+class AppleAuthRepository implements IAuthRepository {
+  AppleAuthRepository(this.ref);
+  final Ref ref;
+  @override
+  Future<void> signIn() async {
+    final signInAccount = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+    );
+    final credential = AppleAuthProvider.credential(
+      signInAccount.authorizationCode,
+    );
+
+    ref.read(signInRepositoryProvider(credential: credential));
+  }
+}
--- lib/features/auth/presentation/controllers/auth_controller.dart
+++ lib/features/auth/presentation/controllers/auth_controller.dart
@@ -1,5 +1,7 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:we_travel/features/auth/data/repositories/google_auth_repository.dart';
+
+import '../../data/repositories/apple_auth_repository.dart';
+import '../../data/repositories/google_auth_repository.dart';
 
 part 'auth_controller.g.dart';
 
@@ -8,9 +10,17 @@ class AuthController extends _$AuthController {
   @override
   FutureOr<void> build() {}
 
+  // Googleサインイン
   Future<void> signInWithGoogle() async {
     state = await AsyncValue.guard(() async {
       await ref.read(googleAuthRepositoryProvider).signIn();
+    });
+  }
+
+  // Appleサインイン
+  Future<void> signInWithApple() async {
+    state = await AsyncValue.guard(() async {
+      await ref.read(appleAuthRepositoryProvider).signIn();
     });
   }
 }
