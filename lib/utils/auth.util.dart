7ca960d2d40f1fab9494506d48680974ad5c3747
onishi taisei <taisei.developer@gmail.com>
2022-11-19 19:33:49 +0900

    🚀 feat: 汎用的に使うFirebaseAuthをutilsに作成（置き場所がわからなかったので一旦ここに）

---
+++ lib/utils/auth.util.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/// 汎用的に使うauthの置き場所がわからなかったので
+/// 一旦ここに置いてます
+
+final authProvider = Provider<FirebaseAuth>((_) => FirebaseAuth.instance);
