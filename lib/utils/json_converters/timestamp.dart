5f4e284e34dd5822eb9116ccc75da128f1f6072f
onishi taisei <taisei.developer@gmail.com>
2022-11-22 14:09:13 +0900

    🚀 feat: DateTime ⇄ TimestampのConverterを作成

---
+++ lib/utils/json_converters/timestamp.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+const timestampConverter = TimestampConverter();
+
+class TimestampConverter implements JsonConverter<DateTime, Timestamp> {
+  const TimestampConverter();
+
+  @override
+  DateTime fromJson(Timestamp json) => json.toDate();
+
+  @override
+  Timestamp toJson(DateTime object) => Timestamp.fromDate(object);
+}
